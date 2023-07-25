6d485c45fd89f0cf1ca2ee91896deac7974a5315
Andrew croshaw <120042631+A-Croshaw@users.noreply.github.com>
2023-07-25 22:45:50 +0100

    changed to scroll

--- assets/script.js
+++ assets/script.js
@@ -67,11 +67,11 @@ function pageLoad(){
 function disableScroll() {
     // Get the current page scroll position in the vertical direction
    let scrollTop =
-       window.pageYOffset || document.documentElement.scrollTop;
+       window.scrollY || document.documentElement.scrollTop;
            
 // Get the current page scroll position in the horizontal direction 
  let scrollLeft =
-   window.pageXOffset || document.documentElement.scrollLeft;
+   window.scrollX || document.documentElement.scrollLeft;
    
   // if any scroll is attempted,
  // set this to the previous value
